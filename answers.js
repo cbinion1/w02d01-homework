aac41ea508e4997d5be15010adcc7ce13e83804a
mac <mac@macs-MacBook-Pro.local>
2018-06-18 17:12:43 -0600

    1st Commit--Selecting datatypes

---
+++ answers.js
@@ -0,0 +1,52 @@
+//Datatypes:
+
+//1. I would use a Boolean for the light switch as a Boolean is represented by "true" or "false", which 
+//would correlate to a switch's on or off.
+	let lightOn = true;
+//2. The datatype for an user's email adddres would be a string as it may contain letters and numbers
+	let address = "sunsideup@noemail.com"
+//3.  The datatype for a spaceship would be an object, as different aspects of the ship would be 
+//elements such as lasers, turrets, and other parts of the ship that lend to the individual aspect of the 
+//ship.
+	const ship = {
+		lasers: 4,
+		meson guns: 18,
+		shields: true
+	}
+//4. The datatype for a list of student names would be an array.  
+	let students = ["johnny", "mary", "alf", "frankie"]
+//5. The datatype for a list of student names, each with a location would also be a nested array.
+	const students1 =[
+		name: "Maria",
+		location: "Mars U"
+	]
+	[
+		name: "Francoise"
+		location: "Schenectady"
+	]
+	[
+		name: "Aloishus"
+		location: "Eureka"
+	]
+//6. The datatype for a list of student names, each with a list of location and tv shows would be an array
+//of objects.
+	const students2 = {
+		[name: "Chucky",
+		location: "Behind you",
+		favoriteMovies: ["Bride of Chucky", "Child's Play", "Child's Play 2"]
+		]
+		[name: "Bruce",
+		location: "Elk Grove",
+		favoriteMovies: "Evil Dead", "Evil Dead 2", "Ash vs. The Army of Darkness"
+		]
+		[name: "Doc Brown",
+		location: "Hill Valley",
+		favoriteMovies: "Back to the Future", "Back to the Future 2", "Hill Valley Slasher"]
+		
+	}
+
+
+
+
+
+
