b2828317c4b2aa935287bbd4888504655f07e1c3
mac <mac@macs-MacBook-Pro.local>
2018-06-18 17:54:06 -0600

    4th commit - accessed limbo

--- answers.js
+++ answers.js
@@ -97,7 +97,30 @@ const crazyObject = {
 	console.log(crazyObject.larry.characters[1]);
 
 
-////
+////Object-Ception:
+
+
+const inception = {
+   reality: {
+       dreamLayer1: {
+           dreamLayer2: {
+               dreamLayer3: {
+                   dreamLayer4: {
+                       dreamLayer5: {
+                           dreamLayer6: {
+                               limbo: "Joseph Gordon Levitt"
+                          }
+                       }
+                   }
+               }
+           }
+       }
+   }
+}
+
+inception.reality.dreamLayer1.dreamLayer2.dreamLayer3.dreamLayer4.dreamLayer5.dreamLayer6.limbo = "Null";
+
+console.log(inception);
 
 
 
